480e03167062f1987f9c6888412f207c35287bd5
lewisyng <lewisyoung1306@yahoo.de>
2022-03-07 00:31:24 +0100

    add footer

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -4,17 +4,21 @@ import Heading from '../UI/Heading/Heading'
 
 export const Projects = () => {
   return (
-    <>
-      <div className="border-[8px] bg-[#0c0c0c] text-[#fce8ce] border-[#0c0c0c] p-4">
-        <Heading variant="h4" className="text-[#e06d8a]">Woran ich arbeite</Heading>
+    <div className="mb-20">
+      <div className="border-[8px] border-[#0c0c0c] bg-[#0c0c0c] p-4 text-white">
+        <Heading variant="h4">
+          Woran ich arbeite
+        </Heading>
+
         <Heading variant="h2">Meine Projekte</Heading>
       </div>
-      <div className="mt-8 grid gap-4 grid-cols-1 md:grid-cols-2">
+
+      <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2">
         {projects.map((project: any, idx: number) => (
-            <Project project={project} key={idx} />
+          <Project project={project} key={idx} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 

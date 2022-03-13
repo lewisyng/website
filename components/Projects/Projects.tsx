2380a57e2418ad5cac62e1a7cc8360859f523d6d
lewisyng <lewisyoung1306@yahoo.de>
2022-03-13 21:06:44 +0100

    lint

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,7 +1,9 @@
 import Project from '../Project/Project'
 import Heading from '../UI/Heading/Heading'
 
-export const Projects = ({ projects }) => {
+export const Projects = ({ projects }: {
+  projects: any
+}) => {
   return (
     <div className="mb-20">
       <div className="prose max-w-full border-[8px] border-[#0c0c0c] bg-[#0c0c0c] p-4 text-center">
